b5c5119a1020c808181fef73edb53a957cec7ccd
phantom-sage <z4gamdev@gmail.com>
2022-03-08 08:21:00 +0200

    integration 3

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -19,8 +19,8 @@ class UserResource extends JsonResource
             'name' => $this->name,
             'username' => $this->username,
             'email' => $this->email,
-            'created_at' => $this->created_at,
-            'updated_at' => $this->updated_at,
+            'created_at' => $this->created_at->format('Y-m-d h:i:s'),
+            'updated_at' => $this->updated_at->format('Y-m-d h:i:s'),
         ];
     }
 }
